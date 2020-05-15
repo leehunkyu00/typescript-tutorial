995aea13f9292ccf3408f86638d0b8ee50aa282b
hklee <leehunkyu00@gmail.com>
2020-05-15 15:27:46 +0900

    basic code for typescript

---
+++ basic3/person.ts
@@ -0,0 +1,14 @@
+export class Person {
+    protected name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    sayHello() {
+        return "Hello, " + this.name + "haha";
+    }
+}
+
+// const person = new Person("Lee");
+// console.log(person.sayHello());
